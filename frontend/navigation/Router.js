1ed4d125c68b988f7b001d7ebb35b0bccf667f3f
YigitEkin <yigit.ekin@ug.bilkent.edu.tr>
2022-12-16 23:02:26 +0300

    [ui] - added drawer navigation

--- frontend/navigation/Router.js
+++ frontend/navigation/Router.js
@@ -1,128 +1,73 @@
 import React from "react";
 import { Text } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { useAuthStore } from "../stores/useAuthStore";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import "react-native-gesture-handler";
 
-const NotLoggedInRoutes = [
+const routes = [
   {
-    name: "SignUpMail",
-    component: () => <Text>SignUpMail</Text>,
-    screenOptions: {
-      headerShown: true,
-      headerBackTitleVisible: false,
-      headerTintColor: "#fff",
-      headerTitle: "",
-      headerStyle: {
-        backgroundColor: "#000",
-        shadowColor: "transparent",
-        height: 0,
-      },
-    },
+    name: "Home",
+    component: () => (
+      <Text>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis eos
+        itaque aliquam. Et vero excepturi nesciunt debitis, fugiat distinctio
+        iusto illum aliquam, a quae incidunt id necessitatibus nisi itaque
+        similique numquam expedita consequatur modi aliquid nemo reprehenderit
+        facere consectetur. Deleniti iste a earum neque quas minus quam amet
+        tenetur voluptate.
+      </Text>
+    ),
+    props: {},
   },
   {
-    name: "HomePage",
-    component: () => <Text>Home</Text>,
-    screenOptions: {
-      headerShown: false,
-    },
-  },
-];
-
-const LoggedInRoutes = [
-  {
-    name: "SignUpMail",
-    component: () => <Text>SignUpMail2</Text>,
-    screenOptions: {
-      headerShown: true,
-      headerBackTitleVisible: false,
-      headerTintColor: "#fff",
-      headerTitle: "",
-      headerStyle: {
-        backgroundColor: "#fff",
-        shadowColor: "transparent",
-        height: 0,
-      },
-    },
-  },
-  {
-    name: "HomePage",
-    component: () => <Text>HomePage2</Text>,
-    screenOptions: {
-      headerShown: false,
-    },
+    name: "Battery",
+    component: () => (
+      <Text>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae modi
+        eaque voluptates id ullam aspernatur deserunt atque officia, illo
+        cumque?
+      </Text>
+    ),
+    props: {},
   },
 ];
 
-const Stack = createNativeStackNavigator();
-const Tab = createBottomTabNavigator();
-
-function NonAuthRouter() {
-  return (
-    <Stack.Navigator
-      initialRouteName="HomePage"
-      screenOptions={{
-        contentStyle: {
-          backgroundColor: "#fff",
-          width: "100%",
-        },
-      }}
-    >
-      {NotLoggedInRoutes.map((route, index) => (
-        <Stack.Screen
-          options={{
-            ...route.screenOptions,
-            contentStyle: {
-              backgroundColor: "#fff",
-              width: "100%",
-              alignItems: "center",
-            },
-          }}
-          key={index}
-          name={route.name}
-          component={route.component}
-        />
-      ))}
-    </Stack.Navigator>
-  );
-}
-
-function AuthRouter() {
-  return (
-    <Tab.Navigator
-      initialRouteName="HomePage"
-      screenOptions={{
-        contentStyle: {
-          backgroundColor: "#fff",
-          width: "100%",
-        },
-      }}
-    >
-      {LoggedInRoutes.map((route, index) => (
-        <Tab.Screen
-          options={{
-            ...route.screenOptions,
-            contentStyle: {
-              backgroundColor: "#fff",
-              width: "100%",
-              alignItems: "center",
-            },
-          }}
-          key={index}
-          name={route.name}
-          component={route.component}
-        />
-      ))}
-    </Tab.Navigator>
-  );
-}
+const Drawer = createDrawerNavigator();
 
 export default function Router() {
-  const user = useAuthStore((state) => state.user);
   return (
     <NavigationContainer>
-      {user ? <AuthRouter /> : <NonAuthRouter />}
+      <Drawer.Navigator>
+        {routes.map((route) => (
+          <Drawer.Screen
+            key={route.name}
+            name={route.name}
+            component={route.component}
+            options={{
+              drawerActiveBackgroundColor: "#555",
+              drawerActiveTintColor: "#0f0",
+              drawerContentStyle: {
+                backgroundColor: "#000",
+              },
+              drawerLabelStyle: {
+                fontSize: 20,
+              },
+              drawerInactiveTintColor: "#fff",
+              drawerType: "slide",
+              drawerStatusBarAnimation: "slide",
+              headerStyle: {
+                backgroundColor: "#000",
+              },
+              headerTintColor: "#fff",
+              sceneContainerStyle: {
+                backgroundColor: "#000",
+              },
+              headerTitle: () => null,
+            }}
+            {...route.props}
+          />
+        ))}
+      </Drawer.Navigator>
     </NavigationContainer>
   );
 }
